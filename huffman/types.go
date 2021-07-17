a6ef2a25005f4a6d57543d4cf875bd09b45cb7bb
koukyo1994 <arabiannight1994@yahoo.co.jp>
2021-07-17 15:28:18 +0900

    :sparkles: NodesHeapを追加

--- huffman/types.go
+++ huffman/types.go
@@ -14,3 +14,29 @@ type Code struct {
 	Bit   rune
 	Next  *Code
 }
+
+type NodesHeap []Node
+
+func (h NodesHeap) Len() int {
+	return len(h)
+}
+
+func (h NodesHeap) Less(i, j int) bool {
+	return h[i].Freq < h[j].Freq
+}
+
+func (h NodesHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *NodesHeap) Push(x interface{}) {
+	*h = append(*h, x.(Node))
+}
+
+func (h *NodesHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
